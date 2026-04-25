03a08193b943169b183d91987c561e1ee85e65a1
Nils-dev-Mertens <nilsm2950@gmail.com>
2026-04-25 23:05:24 +0200

    try to update to let the artifact work or not

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? [["github"], ["html"]] : "html",
   use: {
     baseURL: "http://localhost:4321",
     trace: "on-first-retry",
